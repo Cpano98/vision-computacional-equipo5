4fb54a5a49ad70a4804db91ac0b002773933ba22
Benjamin Cisneros Barraza <cisnerosbarraza@gmail.com>
2025-09-21 19:36:12 -0600

    Reporte y zip generados

--- 2.5_AlgoritmosDeMejoramiento/package.sh
+++ 2.5_AlgoritmosDeMejoramiento/package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zip 2_2_Algoritmos_de_mejoramiento_Equipo5.zip 2.5_Algoritmos_de_mejoramiento.ipynb reporte/2.5_Algoritmos_de_mejoramiento.pdf data/*
+zip 2_5_Algoritmos_de_mejoramiento_Equipo5.zip 2.5_Algoritmos_de_mejoramiento.ipynb reporte/2.5_Algoritmos_de_mejoramiento.pdf data/*
